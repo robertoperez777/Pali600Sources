ca5d4f68be3232e251c416851305691494d64cbe
kecskeméti Pál <kecskemetip@gmail.com>
2012-11-11 13:24:54 +0100

    Spring start fix

--- FileMakerParser/src/main/java/org/kep/filemakerparser/service/AddressServiceImpl.java
+++ FileMakerParser/src/main/java/org/kep/filemakerparser/service/AddressServiceImpl.java
@@ -1,9 +1,5 @@
 package org.kep.filemakerparser.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional
 public class AddressServiceImpl implements AddressService {
 }
