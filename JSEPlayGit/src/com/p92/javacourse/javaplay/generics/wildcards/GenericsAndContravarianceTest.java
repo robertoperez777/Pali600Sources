840120df88805ea55f65cb0255b832f1f92437a7
Kecskeméti Pál <kecskemetip@gmail.com>
2014-01-24 09:32:41 +0100

    Spring AOP Tutorial - Mkyong

---
+++ JSEPlayGit/src/com/p92/javacourse/javaplay/generics/wildcards/GenericsAndContravarianceTest.java
@@ -0,0 +1,18 @@
+package com.p92.javacourse.javaplay.generics.wildcards;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericsAndContravarianceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTest() {
+		GenericsAndContravariance gac = new GenericsAndContravariance();
+		gac.test();
+	}
+
+}
